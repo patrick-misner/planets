874cc5b8b20fb48cefc1742cb05b5664342dc215
Patrick Misner <misner.patrick@gmail.com>
2022-06-08 13:54:53 -0600

    test

--- client/app/env.js
+++ client/app/env.js
@@ -1,2 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'dev-in-9pjrc.us.auth0.com'
+export const audience = 'https://patrickdev.com'
+export const clientId = 'KCkMZ4ZIRzme4LvLXEQoEorQQItsONAx'
